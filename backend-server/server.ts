47f726afbd8f3043d29fe8c46c2f889d8649f2f7
unknown <huzmanbutt@gmail.com>
2023-09-16 05:44:44 -0700

    dockerized backend w docker-compose

--- backend-server/server.ts
+++ backend-server/server.ts
@@ -37,8 +37,9 @@ app.use((err : unknown, req : Request, res : Response, next : NextFunction) => {
 
 try {
  
-  mongoose.connect(process.env.MONGO_URL) ;
-  console.log("DATABASE CONNECTED");
+  mongoose.set('strictQuery', true);
+  mongoose.connect(process.env.MONGO_URI) ;
+  console.log(`DATABASE CONNECTED   on ${process.env.MONGO_URI} `);
   app.listen(process.env.PORT, () => {
     console.log(`LISTENING TO PORT ${process.env.PORT}`);
   });
